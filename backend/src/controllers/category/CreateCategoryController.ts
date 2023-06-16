44d6a0e90d07468cc489428c5483a6d3dab4ebdd
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-06-16 15:24:35 -0300

    Update

--- backend/src/controllers/category/CreateCategoryController.ts
+++ backend/src/controllers/category/CreateCategoryController.ts
@@ -5,10 +5,12 @@ class CreateCategoryController {
   async handle(req: Request, res: Response) {
     const { name } = req.body;
     const createCategoryService = new CreateCategoryService();
-
-    const category = await createCategoryService.execute({ name });
+    const category = await createCategoryService.execute({
+      name,
+    });
 
     return res.json(category);
   }
 }
+
 export { CreateCategoryController };
