7e6ab547021495178bdca31d5a5f76c089feefba
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-06-26 12:01:00 -0300

    Update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,30 +3,47 @@ import AppBar from "@/components/appBar";
 import { Login } from "@/components/login/login";
 import { Button } from "@/components/Button/button";
 import Link from "next/link";
-import { AuthContext } from "@/contexts/AuthContent";
-import Provider from "../components/Provider";
+import { getSession, signIn } from "next-auth/react";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    signIn("credentials");
+  };
   return (
-    <Provider>
-      <main>
-        <div className="bg-white font-sans text-red-500 mx-auto p-4">
-          <AppBar />
-        </div>
-        <div className="m-8 w-auto h-1/2 bg-white text-red-500 flex flex-col text-center items-center">
-          <h1>Login</h1>
-          <form className="flex flex-col m-3">
-            <Login placeholder="Digite seu email" type="text" />
-            <Login placeholder="Digite sua senha" type="password" />
-            <Button type="submit" loading={false}>
-              Acessar
-            </Button>
-          </form>
-          <Link href="/signup" className="cursor-pointer">
-            Não possui uma conta? Cadastre-se agora!
-          </Link>
-        </div>
-      </main>
-    </Provider>
+    <main>
+      <div className="bg-white font-sans text-red-500 mx-auto p-4">
+        <AppBar />
+      </div>
+      <div className="m-8 w-auto h-1/2 bg-white text-red-500 flex flex-col text-center items-center">
+        <h1>Login</h1>
+        <form onSubmit={onSubmit} className="flex flex-col m-3">
+          <Login placeholder="Digite seu email" type="text" />
+          <Login placeholder="Digite sua senha" type="password" />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link href="/signup" className="cursor-pointer">
+          Não possui uma conta? Cadastre-se agora!
+        </Link>
+      </div>
+    </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/new",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+};
--- frontend/src/app/signup/_app/_app.tsx
+++ frontend/src/app/signup/_app/_app.tsx
@@ -1,11 +1,11 @@
 import { AppProps } from "next/app";
-import { AuthProvider } from "../../../contexts/AuthContent";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <AuthProvider>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </AuthProvider>
+    </SessionProvider>
   );
 }
 
