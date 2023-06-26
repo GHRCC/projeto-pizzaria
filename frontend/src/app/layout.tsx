bceff937078fc8fc2cb376a7611603da87d94a51
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-06-26 13:23:03 -0300

    Update com Auth

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import NextAuthSessionProvider from "./providers/sesssionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,49 +1,53 @@
-import { useContext, FormEvent } from "react";
-import AppBar from "@/components/appBar";
-import { Login } from "@/components/login/login";
-import { Button } from "@/components/Button/button";
-import Link from "next/link";
-import { getSession, signIn } from "next-auth/react";
-import { GetServerSideProps } from "next";
+// "use client";
+// import { useContext, FormEvent } from "react";
+// import AppBar from "@/components/appBar";
+// import { Login } from "@/components/login/login";
+// import { Button } from "@/components/Button/button";
+// import Link from "next/link";
+// import { getSession, signIn } from "next-auth/react";
+// import { GetServerSideProps } from "next";
+
+// export default function Home() {
+//   const onSubmit = (e: FormEvent) => {
+//     e.preventDefault();
+//     signIn("credentials");
+//   };
+//   return (
+//     <main>
+//       <div className="bg-white font-sans text-red-500 mx-auto p-4">
+//         <AppBar />
+//       </div>
+//       <div className="m-8 w-auto h-1/2 bg-white text-red-500 flex flex-col text-center items-center">
+//         <h1>Login</h1>
+//         <form onSubmit={onSubmit} className="flex flex-col m-3">
+//           <Login placeholder="Digite seu email" type="text" />
+//           <Login placeholder="Digite sua senha" type="password" />
+//           <button type="submit">Acessar</button>
+//         </form>
+//         <Link href="/signup" className="cursor-pointer">
+//           Não possui uma conta? Cadastre-se agora!
+//         </Link>
+//       </div>
+//     </main>
+//   );
+// }
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 
 export default function Home() {
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    signIn("credentials");
-  };
+  const { data: session, status } = useSession({
+    required: true,
+  });
+
+  if (status === "loading") {
+    return <></>;
+  }
+
   return (
-    <main>
-      <div className="bg-white font-sans text-red-500 mx-auto p-4">
-        <AppBar />
-      </div>
-      <div className="m-8 w-auto h-1/2 bg-white text-red-500 flex flex-col text-center items-center">
-        <h1>Login</h1>
-        <form onSubmit={onSubmit} className="flex flex-col m-3">
-          <Login placeholder="Digite seu email" type="text" />
-          <Login placeholder="Digite sua senha" type="password" />
-          <button type="submit">Acessar</button>
-        </form>
-        <Link href="/signup" className="cursor-pointer">
-          Não possui uma conta? Cadastre-se agora!
-        </Link>
-      </div>
-    </main>
+    <div className="flex h-screen items-center justify-center">
+      This is a protected page.
+      <button onClick={() => signOut()}>Deslogar</button>
+    </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  if (session) {
-    return {
-      redirect: {
-        destination: "/new",
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: {
-      session,
-    },
-  };
-};
